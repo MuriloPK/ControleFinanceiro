d6c1acda6e748e71c139ff94efaab29c64b8764d
MuriloPK <murilosouzadecarvalho@gmail.com>
2024-07-03 10:18:02 -0300

    implementa repository

--- src/main/java/com/clubeselvas/ControleFinanceiro/infra/MensalidadeInfraRepository.java
+++ src/main/java/com/clubeselvas/ControleFinanceiro/infra/MensalidadeInfraRepository.java
@@ -2,11 +2,29 @@ package com.clubeselvas.ControleFinanceiro.infra;
 
 import com.clubeselvas.ControleFinanceiro.application.repository.MensalidadeRepository;
 import com.clubeselvas.ControleFinanceiro.domain.entity.Mensalidade;
+import com.clubeselvas.ControleFinanceiro.handler.APIException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Log4j2
+@RequiredArgsConstructor
 public class MensalidadeInfraRepository implements MensalidadeRepository {
+
+    private MensalidadeSpringDataJpaRepository mensalidadeSpringDataJpaRepository;
 
     @Override
     public Mensalidade salva(Mensalidade mensalidade) {
-        return null;
+        log.info("[inicia]MensalidadeInfraRepository - salva");
+        try {
+            mensalidadeSpringDataJpaRepository.save(mensalidade);
+        } catch (DataIntegrityViolationException e) {
+            throw APIException.build(HttpStatus.BAD_REQUEST, "Existe campos invalidos!", e);
+        }
+        log.info("[finaliza]MensalidadeInfraRepository - salva");
+        return mensalidade;
     }
 }
--- src/main/java/com/clubeselvas/ControleFinanceiro/infra/MensalidadeSpringDataJpaRepository.java
+++ src/main/java/com/clubeselvas/ControleFinanceiro/infra/MensalidadeSpringDataJpaRepository.java
@@ -1,2 +1,9 @@
-package com.clubeselvas.ControleFinanceiro.infra;public interface MensalidadeSpringDataJpaRepository {
+package com.clubeselvas.ControleFinanceiro.infra;
+
+import com.clubeselvas.ControleFinanceiro.domain.entity.Mensalidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MensalidadeSpringDataJpaRepository extends JpaRepository<Mensalidade, UUID> {
 }
